09c7ebfcae4483b8a2e07698d8857cfe3db03926
UPC-Skylar <zaynml2021@gmail.com>
2025-05-14 05:10:01 -0500

    add nav-bar sections landing page

---
+++ src/app/domains/products/components/caregivers/caregivers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caregivers',
+  imports: [],
+  templateUrl: './caregivers.component.html',
+  styleUrl: './caregivers.component.css'
+})
+export class CaregiversComponent {
+
+}
---
+++ src/app/domains/products/components/family/family.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-family',
+  imports: [],
+  templateUrl: './family.component.html',
+  styleUrl: './family.component.css'
+})
+export class FamilyComponent {
+
+}
