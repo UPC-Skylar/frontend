29a942153cef57ca30be7af42cea6b3cf4bf0531
UPC-Skylar <zaynml2021@gmail.com>
2025-05-14 10:19:32 -0500

    add about us interface

--- src/app/core/components/about/about.component.ts
+++ src/app/core/components/about/about.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    TranslatePipe,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+  }
 }
