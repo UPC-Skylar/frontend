47f0ad2976c6e07494b4321553cc748e736ffa1b
UPC-Skylar <zaynml2021@gmail.com>
2025-05-14 08:51:15 -0500

    add caregivers section, interface, images and description

--- src/app/domains/products/components/caregivers/caregivers.component.ts
+++ src/app/domains/products/components/caregivers/caregivers.component.ts
@@ -14,5 +14,30 @@ import {TranslatePipe} from '@ngx-translate/core';
   styleUrl: './caregivers.component.css'
 })
 export class CaregiversComponent {
+  caregiversList = [
+    {
+      name: 'Ana María López',
+      description: 'Cuidé a varios adultos mayores con Alzheimer y personas con movilidad reducida. Me encanta hacerlos sentir acompañados.',
+      image: 'assets/images/cuidador-resenia.avif'
+    },
+    {
+      name: 'Carlos Pérez',
+      description: 'Soy un cuidador profesional con más de 5 años de experiencia. Me especializo en cuidados post-quirúrgicos y personas con discapacidad.',
+      image: 'assets/images/cuidador-resenia2.jpg'
+    },
+    {
+      name: 'Luis Fernández',
+      description: 'Cuidé a personas con enfermedades terminales, proporcionando apoyo emocional y físico para ellos y sus familias.',
+      image: 'assets/images/cuidador-resenia3.jpg'
+    }
+  ];
 
+  constructor() {
+    setInterval(() => {
+      const first = this.caregiversList.shift();
+      if (first) {
+        this.caregiversList.push(first);
+      }
+    }, 3000);
+  }
 }
